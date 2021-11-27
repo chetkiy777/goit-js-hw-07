e903d0141a36081a86e1de0d2210c62383a77315
Igor Gladkiy <egunka1988@gmail.com>
2021-11-27 17:29:50 +0200

    add images

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+const addGallery = (imgArray) => {
+    const galleryList = document.querySelector('.gallery__list')
+    
+    for (let i = 0; i < imgArray.length; i += 1) {
+       
+        let liElem = document.createElement('li')
+        liElem.classList.add('gallery__item')
+        liElem.innerHTML = `<img src="${imgArray[i].preview}">`
+        let spanElem = document.createElement('span')
+        spanElem.textContent = `${imgArray[i].description}`
+        liElem.appendChild(spanElem)
+        galleryList.appendChild(liElem)
+        console.log(liElem)
+    }
+}
+
 console.log(galleryItems);
+
+addGallery(galleryItems)
